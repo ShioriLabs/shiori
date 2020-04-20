732a3ce29ff58341e1ddd1d4764f53e97c9f9394
Dhika Rizky <deka_r@outlook.com>
2020-04-21 00:52:34 +0800

    Added encourage command

--- src/module/fun/index.ts
+++ src/module/fun/index.ts
@@ -28,7 +28,8 @@ const Confess = new Resolver('confess', (message: Message, args: string[]) => {
   const possibleString = [
     'do you want to be my lover?',
     'would you go out with me?',
-    'can we go on a date together?'
+    'can we go on a date together?',
+    'can i love you?'
   ]
 
   const confession = possibleString[Math.floor(Math.random() * possibleString.length)]
@@ -46,9 +47,31 @@ const Coin = new Resolver('coin', (message: Message) => {
   message.channel.send(random > 0.5 ? 'Head' : 'Tails')
 }, 'Flip a coin')
 
+const Encourage = new Resolver('encourage', (message: Message) => {
+  const user = message.mentions.users.first()
+
+  const possibleString = [
+    'you can do it!',
+    'don\'t let your dreams be dreams. So just do it!',
+    'don\'t give up!',
+    'I\'m proud of you! You can do it!',
+    '頑張って！',
+    '加油！'
+  ]
+
+  const encouragement = possibleString[Math.floor(Math.random() * possibleString.length)]
+
+  if (user) {
+    message.channel.send(`<@!${user.id}>, ${encouragement}`)
+  } else {
+    message.channel.send('Please mention someone you want to encourage!')
+  }
+}, 'Encourage someone to do their best!')
+
 export default [
   // WuohMantab,
   Greet,
   Confess,
-  Coin
+  Coin,
+  Encourage
 ]
