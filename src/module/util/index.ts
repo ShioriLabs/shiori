75a8a4c94e7fa4aede0bc135f330e8a22e82edaf
Dhika Rizky <deka_r@outlook.com>
2020-04-20 17:46:04 +0800

    Created a loading message for wikipedia search

--- src/module/util/index.ts
+++ src/module/util/index.ts
@@ -5,13 +5,14 @@ import Resolver from '../../class/Resolver'
 import WikipediaAPIResponse from '../../class/WikipediaAPIResponse'
 
 const Define = new Resolver('define', async (message: Message, args: string[]) => {
+  const sentMessage = await message.channel.send('Let me search Wikipedia for that...')
   if (args.length === 0) {
     message.channel.send('You need to insert something to search!')
   } else {
     const query = encodeURI(args.join(' '))
     const { data: searchResult } = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&utf8=&format=json`)
     if (searchResult.query.search.length === 0) {
-      message.channel.send(`Can't find \`${query}\` in Wikipedia. Please try again`)
+      await sentMessage.edit(`Can't find \`${query}\` in Wikipedia. Please try again`)
       return
     }
     const { data } = await axios.get(`https://en.wikipedia.org/w/api.php?&action=query&prop=extracts|info&titles=${encodeURI(searchResult.query.search[0].title)}&format=json&inprop=url&explaintext=true&exlimit=1&exintro=true`)
@@ -22,7 +23,8 @@ const Define = new Resolver('define', async (message: Message, args: string[]) =
       .setURL(page.fullurl)
       .setDescription(page.extract)
       .setFooter('Content from Wikipedia')
-    message.channel.send(resultMessage)
+    await sentMessage.edit('Here\'s what I found on Wikipedia:')
+    await sentMessage.edit(resultMessage)
   }
 }, 'Get a definition of something from Wikipedia')
 
