047cf21209cd10b5fb16f04db7e2ef8031ed07d6
Dhika Rizky <deka_r@outlook.com>
2020-05-01 23:24:05 +0800

    Added try-catch block on ACG commands

--- src/module/acg/index.ts
+++ src/module/acg/index.ts
@@ -11,26 +11,30 @@ const Staff = new Resolver('staff', async (message: Message, args: string[]) => 
   }
   const searchQuery = args.join(' ')
   const sentMessage = await message.channel.send(`Searching ${searchQuery} on AniList...`)
-  const result = await anilist.getStaff(searchQuery)
+  try {
+    const result = await anilist.getStaff(searchQuery)
 
-  if (!result) {
+    if (!result) {
+      await sentMessage.edit(`Can't find ${searchQuery} on AniList!`)
+      return
+    }
+
+    const descriptionSections = result.description ? result.description.split('\n\n') : ['No description available.']
+    const slicedDescription = descriptionSections.slice(0, 3).join('\n\n')
+    const reformattedDescription = slicedDescription.replace(/__/g, '**')
+    const embedMessage = new MessageEmbed()
+      .setColor('#ffaaa5')
+      .setTitle(`${result.name.full} (${result.name.native})`)
+      .setURL(result.siteUrl)
+      .setDescription(reformattedDescription)
+      .setThumbnail(result.image.medium)
+      .setFooter('Content from AniList', 'https://anilist.co/img/icons/favicon-32x32.png')
+
+    await sentMessage.edit('Here\'s what I found on AniList:')
+    await sentMessage.edit(embedMessage)
+  } catch {
     await sentMessage.edit(`Can't find ${searchQuery} on AniList!`)
-    return
   }
-
-  const descriptionSections = result.description ? result.description.split('\n\n') : ['No description available.']
-  const slicedDescription = descriptionSections.slice(0, 3).join('\n\n')
-  const reformattedDescription = slicedDescription.replace(/__/g, '**')
-  const embedMessage = new MessageEmbed()
-    .setColor('#ffaaa5')
-    .setTitle(`${result.name.full} (${result.name.native})`)
-    .setURL(result.siteUrl)
-    .setDescription(reformattedDescription)
-    .setThumbnail(result.image.medium)
-    .setFooter('Content from AniList', 'https://anilist.co/img/icons/favicon-32x32.png')
-
-  await sentMessage.edit('Here\'s what I found on AniList:')
-  await sentMessage.edit(embedMessage)
 }, 'Get an anime staff\'s detail')
 
 const Anime = new Resolver('anime', async (message: Message, args: string[]) => {
@@ -40,52 +44,56 @@ const Anime = new Resolver('anime', async (message: Message, args: string[]) => 
   }
   const searchQuery = args.join(' ')
   const sentMessage = await message.channel.send(`Searching ${searchQuery} on AniList...`)
-  const result = await anilist.getAnime(searchQuery)
+  try {
+    const result = await anilist.getAnime(searchQuery)
 
-  if (!result) {
+    if (!result) {
+      await sentMessage.edit(`Can't find ${searchQuery} on AniList!`)
+      return
+    }
+
+    const embedMessage = new MessageEmbed()
+      .setColor('#ffaaa5')
+      .setTitle(`${result.title.english ? result.title.english : result.title.romaji}`)
+      .setURL(result.siteUrl)
+      .setDescription(result.description.replace(/<br>/g, ''))
+      .addFields([
+        {
+          name: 'Alternate Name',
+          value: `${result.title.romaji && result.title.english ? `${result.title.romaji}, ` : ''}${result.title.native ? ` ${result.title.native}` : ''}`
+        },
+        {
+          name: 'Season',
+          value: `${result.season} ${result.seasonYear}`,
+          inline: true
+        },
+        {
+          name: 'Episodes',
+          value: result.episodes,
+          inline: true
+        },
+        {
+          name: 'Genres',
+          value: result.genres.join(', '),
+          inline: true
+        },
+        {
+          name: 'Casts',
+          value: result.characters.edges.map(item => {
+            const actor = `[${item.voiceActors[0].name.full}](${item.voiceActors[0].siteUrl})`
+            const character = `[${item.node.name.full}](${item.node.siteUrl})`
+            return `**${actor}**\nas ${character}`
+          }).join('\n\n')
+        }
+      ])
+      .setThumbnail(result.coverImage.medium)
+      .setFooter('Content from AniList', 'https://anilist.co/img/icons/favicon-32x32.png')
+
+    await sentMessage.edit('Here\'s what I found on AniList:')
+    await sentMessage.edit(embedMessage)
+  } catch {
     await sentMessage.edit(`Can't find ${searchQuery} on AniList!`)
-    return
   }
-
-  const embedMessage = new MessageEmbed()
-    .setColor('#ffaaa5')
-    .setTitle(`${result.title.english ? result.title.english : result.title.romaji}`)
-    .setURL(result.siteUrl)
-    .setDescription(result.description.replace(/<br>/g, ''))
-    .addFields([
-      {
-        name: 'Alternate Name',
-        value: `${result.title.romaji && result.title.english ? `${result.title.romaji}, ` : ''}${result.title.native ? ` ${result.title.native}` : ''}`
-      },
-      {
-        name: 'Season',
-        value: `${result.season} ${result.seasonYear}`,
-        inline: true
-      },
-      {
-        name: 'Episodes',
-        value: result.episodes,
-        inline: true
-      },
-      {
-        name: 'Genres',
-        value: result.genres.join(', '),
-        inline: true
-      },
-      {
-        name: 'Casts',
-        value: result.characters.edges.map(item => {
-          const actor = `[${item.voiceActors[0].name.full}](${item.voiceActors[0].siteUrl})`
-          const character = `[${item.node.name.full}](${item.node.siteUrl})`
-          return `**${actor}**\nas ${character}`
-        }).join('\n\n')
-      }
-    ])
-    .setThumbnail(result.coverImage.medium)
-    .setFooter('Content from AniList', 'https://anilist.co/img/icons/favicon-32x32.png')
-
-  await sentMessage.edit('Here\'s what I found on AniList:')
-  await sentMessage.edit(embedMessage)
 }, 'Get the detail of an anime')
 
 export default {
