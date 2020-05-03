2dd6d08ad1548ea8e58d1b076ec10d4acb69cea9
Dhika Rizky <deka_r@outlook.com>
2020-05-04 01:16:44 +0800

    Making balance info command returns actual value

--- src/module/economy/index.ts
+++ src/module/economy/index.ts
@@ -1,13 +1,16 @@
 import { Message, MessageEmbed } from 'discord.js'
 
+import { getBalance } from '../../util/faunaQueries'
+
 import Resolver from '../../class/Resolver'
 
-const Balance = new Resolver('balance', (message: Message) => {
+const Balance = new Resolver('balance', async (message: Message) => {
+  const balance = await getBalance(message.member)
   const messageEmbed = new MessageEmbed()
     .setColor('#ffaaa5')
     .setTitle('Account Statement')
     .addField('Account Holder', message.author.username)
-    .addField('Balance', '0 BCN')
+    .addField('Balance', `${balance} BCN`)
     .setTimestamp(Date.now())
   message.channel.send(messageEmbed)
 }, 'Get balance info')
---
+++ src/util/faunaQueries.ts
@@ -0,0 +1,48 @@
+import fauna from 'faunadb'
+import dotenv from 'dotenv'
+import { GuildMember } from 'discord.js'
+
+import Account from '../class/Account'
+
+dotenv.config()
+
+const q = fauna.query
+const client = new fauna.Client({
+  secret: process.env.FAUNA_TOKEN
+})
+
+const openAccount = async (user: GuildMember): Promise<Account> => {
+  const account: Account = await client.query(
+    q.Create(
+      q.Collection('balance'),
+      {
+        data: {
+          user: user.id,
+          balance: 0
+        }
+      }
+    )
+  )
+
+  return account
+}
+
+const getBalance = async (user: GuildMember): Promise<number> => {
+  try {
+    const account: Account = await client.query(
+      q.Get(
+        q.Match(
+          q.Index('selectByUser'),
+          user.id
+        )
+      )
+    )
+    return account.data.balance
+  } catch {
+    const account = await openAccount(user)
+    return account.data.balance
+  }
+}
+
+export default client
+export { openAccount, getBalance }
