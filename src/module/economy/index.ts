10602a72fc8b2febae0746e552f9944c18f96525
Dhika Rizky <deka_r@outlook.com>
2020-05-04 12:49:25 +0800

    Added send command for sending BCN

--- src/module/economy/index.ts
+++ src/module/economy/index.ts
@@ -1,6 +1,6 @@
 import { Message, MessageEmbed } from 'discord.js'
 
-import { getBalance } from '../../util/faunaQueries'
+import { getBalance, sendMoney } from '../../util/faunaQueries'
 
 import Resolver from '../../class/Resolver'
 
@@ -10,16 +10,68 @@ const Balance = new Resolver('balance', async (message: Message) => {
     .setColor('#ffaaa5')
     .setTitle('Account Statement')
     .addField('Account Holder', message.author.username)
-    .addField('Balance', `${balance} BCN`)
+    .addField('Balance', `${balance.data.balance} BCN`)
     .setTimestamp(Date.now())
   message.channel.send(messageEmbed)
 }, 'Get balance info')
+
+const Send = new Resolver('send', async (message: Message, args: string[]) => {
+  const to = message.mentions.members.first()
+  if (!to) {
+    message.channel.send('You need to specify the user you want to send money to')
+    return
+  }
+  args.shift()
+  if (args.length === 0) {
+    message.channel.send('Please specify the amount you want to send')
+    return
+  }
+  const amount = Number.parseInt(args.shift())
+  try {
+    await sendMoney(message.member, to, amount)
+    const embed = new MessageEmbed()
+      .setColor('#ffaaa5')
+      .setTitle('Money Received!')
+      .setDescription(`${message.author.tag} has send you some money!`)
+      .addFields([
+        {
+          name: 'Received',
+          value: `${amount} BCN`,
+          inline: true
+        },
+        {
+          name: 'From',
+          value: message.author.tag,
+          inline: true
+        }
+      ])
+      .setTimestamp(Date.now())
+    if (args.length > 0) {
+      embed.addFields([
+        {
+          name: 'Message',
+          value: args.join(' '),
+          inline: false
+        }
+      ])
+    }
+    to.user.send(embed)
+  } catch (e) {
+    if (e instanceof Error && e.message === 'Not enough balance') {
+      message.reply(e.message)
+    } else {
+      console.log(e)
+      message.channel.send('I\'m sorry, I can\'t do that right now. Please try again')
+    }
+  }
+}, 'Send money to someone')
 
 export default {
   id: 'economy',
   name: 'Economy',
   description: 'Earn and spend virtual currency',
   commands: [
-    Balance
+    Balance,
+    Send
   ]
 }
