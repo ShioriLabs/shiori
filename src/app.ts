8aae7e7ea69a40f74e713fa46d0b2251cb34ac59
Dhika Rizky <deka_r@outlook.com>
2021-04-22 12:54:02 +0700

    Installed Sentry

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,34 @@
 import dotenv from 'dotenv'
 import Discord from 'discord.js'
+import * as Sentry from '@sentry/node'
 
 import messageHandler from './messageHandler'
 
 dotenv.config()
+Sentry.init({
+  dsn: process.env.SENTRY_TOKEN,
+  tracesSampleRate: 1.0
 
-const client = new Discord.Client()
-
-client.on('ready', () => {
-  if (client.user) {
-    client.user.setPresence({
-      activity: {
-        name: 'your heart',
-        type: 'LISTENING'
-      }
-    })
-    console.log(`Logged in as ${client.user.tag}`)
-  }
 })
 
-client.on('message', messageHandler)
+try {
+  const client = new Discord.Client()
 
-client.login(process.env.DISCORD_TOKEN)
+  client.on('ready', () => {
+    if (client.user) {
+      client.user.setPresence({
+        activity: {
+          name: 'your heart',
+          type: 'LISTENING'
+        }
+      })
+      console.log(`Logged in as ${client.user.tag}`)
+    }
+  })
+
+  client.on('message', messageHandler)
+
+  client.login(process.env.DISCORD_TOKEN)
+} catch (e) {
+  Sentry.captureException(e)
+}
