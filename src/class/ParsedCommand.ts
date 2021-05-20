45e0e70cb0252c11fdcc6fe4c4ed73171fe6efc9
Dhika Rizky <deka_r@outlook.com>
2021-05-20 15:57:56 +0700

    Making command required in ParsedCommand

--- src/class/ParsedCommand.ts
+++ src/class/ParsedCommand.ts
@@ -18,10 +18,9 @@ class ParsedCommand {
    * @param arg Arguments passed with the command
    * @param body Un-parsed command arguments
    */
-  constructor (command?: string, args?: string[], body?: string) {
-    if (command !== undefined) {
-      this.command = command
-    }
+  constructor (command: string, args?: string[], body?: string) {
+    this.command = command
+
     if (args !== undefined) {
       this.args = args
     }
