a7f52aeca5eab7d1a2731900f068449e45ed619e
Dhika Rizky <deka_r@outlook.com>
2021-07-12 22:38:47 +0700

    Removed audio features

--- src/module/index.ts
+++ src/module/index.ts
@@ -2,13 +2,11 @@ import Module from '../class/Module'
 
 import Core from './core'
 import Fun from './fun'
-// import Music from './music'
 import Util from './util'
 import ACG from './acg'
 import Economy from './economy'
 
 export default [
-  // Music,
   Core,
   Fun,
   Util,
